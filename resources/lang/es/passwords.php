5652982787127155021c8a9adbf15d7ea577e9ca
unknown <sebastian.terrazas@ucb.edu.bo>
2025-09-25 16:54:24 -0400

    ahora esta en español el apartado de profile y password

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'password' => 'La contraseña proporcionada es incorrecta.',
+    'reset' => '¡Tu contraseña ha sido restablecida!',
+    'sent' => '¡Se ha enviado el enlace de restablecimiento de contraseña!',
+    'throttled' => 'Por favor espera antes de reintentar.',
+    'token' => 'Este token de restablecimiento de contraseña es inválido.',
+    'user' => 'No se ha encontrado un usuario con ese correo.',
+    'current_password' => 'error',
+    'password_confirmation' => 'error'
+
+];
