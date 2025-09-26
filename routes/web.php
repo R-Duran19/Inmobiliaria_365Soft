c0cc88ab0b50dd99f0db60faeb5abedc837b2d25
unknown <sebastian.terrazas@ucb.edu.bo>
2025-09-26 10:26:11 -0400

    se agrego en las rutas en el web.php

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,18 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::get('proyectos', function () {
+    return Inertia::render('Proyectos');
+})->middleware(['auth', 'verified'])->name('proyectos');
+
+Route::get('terrenos', function () {
+    return Inertia::render('Terrenos');
+})->middleware(['auth', 'verified'])->name('terrenos');
+
+Route::get('categorias', function () {
+    return Inertia::render('Categorias');
+})->middleware(['auth', 'verified'])->name('categorias');
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
