5cd75ac47444398022233bfe8b944db7834a3d4c
unknown <sebastian.terrazas@ucb.edu.bo>
2025-09-26 10:25:26 -0400

    se creo el componente vue de categorias

---
+++ resources/js/pages/Categorias.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { categorias } from '@/routes';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Categorias',
+        href: categorias().url,
+    },
+];
+</script>
+
+<template>
+     <AppLayout :breadcrumbs="breadcrumbs">
+        categorias
+     </AppLayout>
+</template>
