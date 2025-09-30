8273aa9c15a3411fe9fa80d61619f8421daf08d7
unknown <sebastian.terrazas@ucb.edu.bo>
2025-09-30 11:52:30 -0400

    se creo la migracion de terrenos

---
+++ database/migrations/2025_09_26_143616_create_terrenos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('terrenos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('idproyecto');
+            $table->string('ubicacion');
+            $table->string('categoria');
+            $table->string('superficie');
+            $table->decimal('cuota_inicial', 10, 2);
+            $table->decimal('cuota_mensual', 10, 2);
+            $table->decimal('precio_venta', 10, 2);
+            $table->integer('estado')->default(0); 
+            $table->boolean('condicion')->default(true);
+            $table->timestamps();
+
+            $table->foreign('idproyecto')->references('id')->on('proyectos')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('terrenos');
+    }
+};
