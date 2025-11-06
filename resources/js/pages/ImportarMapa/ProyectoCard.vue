f3fab4a7b4fa2a23d37e8126c16b60eb67e39628
Sebas <sebastian.terrazas@ucb.edu.bo>
2025-11-06 15:59:56 -0400

    ahora hay un boton para añadir nuevo dibujo y para editar dibujo

--- resources/js/pages/ImportarMapa/ProyectoCard.vue
+++ resources/js/pages/ImportarMapa/ProyectoCard.vue
@@ -79,11 +79,33 @@ const emit = defineEmits<{
                         <path
                             stroke-linecap="round"
                             stroke-linejoin="round"
-                            d="M12 20h9M16.5 3.5a2.121 2.121 0 013 3L7 19l-4 1 1-4L16.5 3.5z"
+                            d="M12 4v16m8-8H4"
                         />
                     </svg>
 
                     Dibujar mapa
+                </Button>
+                <Button
+                    @click="router.visit(`/editar-mapa-seleccionado/${props.proyecto.id}`)"
+                    class="flex w-full items-center justify-center gap-2 bg-gray-800
+                    text-color-white border border-white hover:bg-gray-600 sm:w-auto"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-4 w-4"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        stroke-width="2"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                        />
+                    </svg>
+
+                    Editar mapa
                 </Button>
             </div>
         </CardHeader>
