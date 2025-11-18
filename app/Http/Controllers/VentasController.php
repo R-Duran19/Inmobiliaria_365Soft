9c77d47e35153c6d40e17945fc635b537be0a6ff
Sebas <sebastian.terrazas@ucb.edu.bo>
2025-11-18 15:41:31 -0400

    se creo el controlador de ventas con metodo POST

--- app/Http/Controllers/VentasController.php
+++ app/Http/Controllers/VentasController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Terreno;
+use App\Models\Venta;
 use Inertia\Inertia;
+use Illuminate\Http\Request;
+
 
 class VentasController extends Controller
 {
@@ -21,4 +24,30 @@ class VentasController extends Controller
             'terreno' => $terreno
         ]);
     }
+
+public function postVenta(Request $request)
+{
+    $data = $request->validate([
+        'idusuario'       => 'required|exists:users,id',
+        'idcliente'       => 'required|exists:clientes,id',
+        'idterreno'       => 'required|exists:terrenos,id',
+
+        'precio_lista'    => 'required|numeric',
+        'descuento'       => 'required|numeric',
+        'precio_venta'    => 'required|numeric',
+        'cuota_inicial'   => 'required|numeric',
+        'total_plan_pago' => 'required|numeric',
+    ]);
+
+    // AQUÍ ESTABA EL ERROR
+    $venta = Venta::create($data);
+
+    return response()->json([
+        'success' => true,
+        'venta' => $venta
+    ], 201);
+}
+
+
+    
 }
